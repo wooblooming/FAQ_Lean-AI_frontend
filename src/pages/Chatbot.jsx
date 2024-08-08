f3934f635031e8ade443ca4973f5dac8f6150d0b
kimgywls <wh980213@naver.com>
2024-08-08 17:28:27 +0900

    comment & landing page update

--- src/pages/Chatbot.jsx
+++ src/pages/Chatbot.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useRef } from 'react';
-import '../bubble.css'; // 말풍선 CSS를 import
+import '../bubble.css'; {/* 말풍선 CSS를 import */}
 
 const Chatbot = () => {
+  {/* useState : 가변적인 상태를 지니고 있을 수 있게 해 줌 */ }
   const [message, setMessage] = useState('');
+
+  {/* useRef : 랜더링 시 내부 변수 유지할 수 있게 해줌 */ }
   const chatBoxRef = useRef(null);
 
+  {/* 메시지 전송 */ }
   const sendMessage = () => {
-    if (message.trim() === "") return;
+    {/* 공백일 경우 */ }
+    if (message.trim() === "") return; 
 
-    // Display user's message in the chat box
+    {/* 채팅 박스에 고객의 메시지 내용 출력 */ }
     if (chatBoxRef.current) {
       chatBoxRef.current.innerHTML += `
         <div class="text-right">
@@ -17,10 +22,10 @@ const Chatbot = () => {
       `;
     }
 
-    // Clear the input field
+    {/* 메시지 전송 창 clear */ }
     setMessage('');
 
-    // Fetch response from the server
+    {/* AI 챗봇 응답 가져오기 */ }
     fetch('/chat', {
       method: 'POST',
       headers: {
@@ -30,7 +35,7 @@ const Chatbot = () => {
     })
       .then(response => response.json())
       .then(data => {
-        // Display bot's response in the chat box
+        {/* 채팅 박스에 챗봇의 응답 메시지 내용 출력 */ }
         if (chatBoxRef.current) {
           chatBoxRef.current.innerHTML += `
             <div class="text-left">
@@ -43,15 +48,24 @@ const Chatbot = () => {
 
   return (
     <div className="max-w-lg mx-auto bg-white border-blue-300 border p-5 rounded-lg shadow-lg font-sans mt-6"> {/* className은 css 설정 내용*/}
+      {/* 챗봇 이미지 */}
       <img src="chatbot.png" alt="Chatbot" className="w-24 h-24 object-cover mx-auto mb-4" />
+
+      {/* 인사 멘트 출력 */}
       <p className="text-center mb-2">안녕하세요 찬혁 떡볶이입니다!<br />
         찬혁 떡볶이에 대해 무엇이 궁금하신가요?
       </p>
+
+      {/* 채팅 박스 */}
       <div
         className="w-full h-72 border border-gray-300 p-2 overflow-y-auto mb-4"
         id="chat-box"
         ref={chatBoxRef}
-      ></div>
+      >
+
+      </div>
+
+      {/* 메시지 창 */}
       <div className="flex justify-between">
         <input
           type="text"
@@ -60,6 +74,8 @@ const Chatbot = () => {
           onChange={(e) => setMessage(e.target.value)}
           placeholder="무엇이든 물어보세요!"
         />
+
+      {/* 메시지 전송 버튼 */}
         <button
           onClick={sendMessage}
           className="flex items-center justify-center p-2 rounded hover:bg-blue-600 text-white"
--- src/pages/StoreIntroduction.jsx
+++ src/pages/StoreIntroduction.jsx
@@ -3,14 +3,23 @@ import { Link } from 'react-router-dom';
 
 const StoreIntroduction = () => {
   return (
-    <div className="bg-white flex flex-col items-center min-h-screen overflow-y-auto relative">     {/* className은 css 설정 내용*/}
+    <div className="bg-white flex flex-col items-center min-h-screen overflow-y-auto relative">
+      {/* className은 css 설정 내용 */}
       <div className="bg-white rounded-lg p-8 w-full max-w-md text-center mb-2">
+        
+        {/* 매장 로고 이미지 또는 상표 이미지 */}
         <img src={`${process.env.PUBLIC_URL}/store.png`} alt="Store" className="mx-auto mb-4 w-64 h-52 object-contain" />
+        
+        {/* 매장 이름 출력*/}
         <p className="font-bold text-2xl">찬혁 떡볶이</p>
       </div>
+
+      {/* 매장 기본정보 출력 */}
       <div className="bg-slate-100 flex flex-col items-center text-center mb-4 w-64 px-2">
         <p className="mb-4 text-xl">영업 시간 : 9:00 ~ 18:00 <br /> 위치 : 서울특별시</p>
       </div>
+
+      {/* 음식점일 경우 가격 출력 */}
       <div className="bg-slate-100 flex flex-col items-center text-center w-64 px-2">
         <p className="mt-2 mb-4 text-xl">메뉴 및 가격 <br />
           매운 떡볶이 : 3000원 <br />
@@ -20,7 +29,9 @@ const StoreIntroduction = () => {
           각 종 튀김 : 700원 <br />
         </p>
       </div>
-      <Link to="/chatbot"> {/* Link : 특정 주소로 넘어갈 수 있게 함 */}
+
+      {/* Link : 특정 주소로 넘어갈 수 있게 함 */}
+      <Link to="/chatbot">
         <img src={`${process.env.PUBLIC_URL}/chatbot.png`} alt="Chatbot" className="fixed bottom-4 right-4 w-24 h-24 object-cover" />
       </Link>
     </div>
