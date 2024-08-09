944953bfb95a87bf64d7b7aa8f31873ed8098901
kimgywls <65155823+kimgywls@users.noreply.github.com>
2024-08-09 13:32:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-// src/App.js
-import React from 'react';
-// src/index.js
-import './index.css'; // Tailwind CSS를 포함한 CSS 파일을 import
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import LandingMenu from './pages/LandingMenu';
-import Login from './pages/Login';
-import FindId from './pages/FindId';
-import FindPassword from './pages/FindPassword';
-import Loading from './pages/Loading'; 
-import StoreIntroduction from './pages/StoreIntroduction'; 
-import Chatbot from './pages/Chatbot'; 
-import IdFinder from './pages/IdFinder';
-import PsFinder from './pages/PsFinder';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingMenu/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/findid" element={<FindId/>} />
-        <Route path="/findpassword" element={<FindPassword/>} />
-        <Route path='/idfinder'element={<IdFinder/>} />
-        <Route path='/PsFinder'element={<PsFinder/>} />
-
-        {/* 임시 라우트 */}
-        <Route path="/mypage" element={<h1>마이페이지</h1>} />
-        <Route path="/notice" element={<h1>공지사항</h1>} />
-        <Route path="/qna" element={<h1>FQA</h1>} />
-
-
-        {/* customer site */}
-        <Route path="/customer" element={<Loading />} />
-        <Route path="/customer-introduce" element={<StoreIntroduction />} />
-        <Route path="/chatbot" element={<Chatbot />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
