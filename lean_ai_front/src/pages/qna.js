1502ceeffca6c225471cd5920dc564581d85a341
kimgywls <65155823+kimgywls@users.noreply.github.com>
2024-08-20 15:53:43 +0900

    modify UI notification & qna (#64)
    * modify UI notification & qna

--- lean_ai_front/src/pages/qna.js
+++ lean_ai_front/src/pages/qna.js
@@ -10,28 +10,32 @@ const QnA = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg">
-        {/* 뒤로가기 버튼과 제목 배치 */}
-        <div className="flex items-center mb-4">
-          <Link href="/mainPageForPresident" className="text-gray-500 focus:outline-none mr-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 19l-7-7 7-7"
-              />
-            </svg>
-          </Link>
-          <h1 className="text-xl font-bold text-centerx">자주 묻는 질문</h1>
-        </div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="max-w-lg mx-auto bg-white border p-5 rounded-lg shadow-lg font-sans mt-4 mb-2 w-10/12 "
+        style={{ minHeight: '550px' }}
+      >
+        <nav className="flex items-center mb-4">
+          {/* 뒤로가기 버튼과 제목 배치 */}
+          <div className="flex items-center mb-4">
+            <Link href="/mainPageForPresident" className="text-gray-500 focus:outline-none mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </Link>
+            <h1 className="text-xl font-bold text-centerx">자주 묻는 질문</h1>
+          </div>
+        </nav>
 
         {/* FAQ 질문 1 */}
         <div className="mb-4">
@@ -42,20 +46,18 @@ const QnA = () => {
             Q1. FAQ 챗봇이 무엇인가요?
             <span
               id="arrow-1"
-              className={`float-right transform ${
-                openFAQ === 1 ? 'rotate-180' : ''
-              }`}
+              className={`float-right transform ${openFAQ === 1 ? 'rotate-180' : ''
+                }`}
             >
               ▼
             </span>
           </button>
           <div
             id="answer-1"
-            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${
-              openFAQ === 1 ? '' : 'hidden'
-            }`}
+            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${openFAQ === 1 ? '' : 'hidden'
+              }`}
           >
-            챗봇을 통해 소비자에게 업장에서 발생할 수 있는 모든 정보를  
+            챗봇을 통해 소비자에게 업장에서 발생할 수 있는 모든 정보를
             제공하는 AI 서비스 입니다.
           </div>
         </div>
@@ -69,18 +71,16 @@ const QnA = () => {
             Q2. FAQ 데이터는 어떻게 넣어요?
             <span
               id="arrow-2"
-              className={`float-right transform ${
-                openFAQ === 2 ? 'rotate-180' : ''
-              }`}
+              className={`float-right transform ${openFAQ === 2 ? 'rotate-180' : ''
+                }`}
             >
               ▼
             </span>
           </button>
           <div
             id="answer-2"
-            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${
-              openFAQ === 2 ? '' : 'hidden'
-            }`}
+            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${openFAQ === 2 ? '' : 'hidden'
+              }`}
           >
             메인 페이지에 있는 FAQ 데이터 등록 버튼을 클릭하시면 데이터를 넣을
             수 있는 페이지로 이동합니다.데이터를 넣으신다음 MUMUL에서 확인한 후 수정사항을
@@ -97,18 +97,16 @@ const QnA = () => {
             Q3. 매장 정보를 수정하고 싶어요!
             <span
               id="arrow-3"
-              className={`float-right transform ${
-                openFAQ === 3 ? 'rotate-180' : ''
-              }`}
+              className={`float-right transform ${openFAQ === 3 ? 'rotate-180' : ''
+                }`}
             >
               ▼
             </span>
           </button>
           <div
             id="answer-3"
-            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${
-              openFAQ === 3 ? '' : 'hidden'
-            }`}
+            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${openFAQ === 3 ? '' : 'hidden'
+              }`}
           >
             매장 정보를 수정하려면 관리자 페이지에서
             직접 수정할 수 있습니다.
@@ -124,18 +122,16 @@ const QnA = () => {
             Q4. 서비스 관련해 문의하고 싶어요
             <span
               id="arrow-4"
-              className={`float-right transform ${
-                openFAQ === 4 ? 'rotate-180' : ''
-              }`}
+              className={`float-right transform ${openFAQ === 4 ? 'rotate-180' : ''
+                }`}
             >
               ▼
             </span>
           </button>
           <div
             id="answer-4"
-            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${
-              openFAQ === 4 ? '' : 'hidden'
-            }`}
+            className={`text-sm bg-gray-50 p-4 rounded-lg mt-2 ${openFAQ === 4 ? '' : 'hidden'
+              }`}
           >
             서비스 관련 문의는 담당자 이메일(ch@lean-ai.com)로 연락해 주시면
             됩니다.
