abeaa413f486fd0feff03285247e87788dda12be
kimgywls <65155823+kimgywls@users.noreply.github.com>
2024-08-22 17:32:18 +0900

    change get to post

--- lean_ai_front/src/pages/mainPageForPresident.js
+++ lean_ai_front/src/pages/mainPageForPresident.js
@@ -9,6 +9,7 @@ const MainPageWithMenu = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true); // 로그인 상태 관리
   const [showLogoutModal, setShowLogoutModal] = useState(false); // 로그아웃 모달 표시 여부 관리
   const [isMobile, setIsMobile] = useState(false); // 모바일 화면 여부 관리
+  const [storeName, setStoreName] = useState(''); // 스토어 이름을 저장할 상태 추가
   const [errorMessage, setErrorMessage] = useState('');
   const [showErrorMessageModal, setShowErrorMessageModal] = useState(false); // 에러 메시지 모달 상태
 
@@ -22,6 +23,7 @@ const MainPageWithMenu = () => {
 
   // 스토어 정보를 가져오는 함수
   const fetchStoreInfo = async () => {
+
     try {
       const token = localStorage.getItem('token'); // 저장된 JWT 토큰을 가져옴
       console.log('Token:', token);
@@ -31,16 +33,19 @@ const MainPageWithMenu = () => {
       }
 
       const response = await fetch('http://127.0.0.1:8000/api/user-stores/', {
-        method: 'GET',
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}` // 토큰을 헤더에 포함
-        }
+          'Authorization': `Bearer ${token}`, // 토큰을 헤더에 포함
+        },
       });
+      
       if (response.status === 401) {
-        // 토큰 만료 또는 인증 실패
+       // 토큰 만료 또는 인증 실패
         setErrorMessage('세션이 만료되었거나 인증에 실패했습니다. 다시 로그인해 주세요.');
         setShowErrorMessageModal(true);
+        localStorage.removeItem('token'); // 만료된 토큰 삭제
+        router.push('/login'); // 로그인 페이지로 리디렉션
         return;
       }
 
@@ -52,7 +57,7 @@ const MainPageWithMenu = () => {
       if (storeData && storeData.length > 0) {
         setStoreName(storeData[0].store_name); // 첫 번째 스토어의 이름을 설정
       } else {
-        setStoreName('No Store Available'); // 스토어가 없을 경우 처리
+        setStoreName(' '); // 스토어가 없을 경우 처리
       }
     } catch (error) {
       console.error('Error:', error);
