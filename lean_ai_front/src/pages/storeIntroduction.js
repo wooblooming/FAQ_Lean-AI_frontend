16d8651b8b4bae89bbde880913734f4be7478c9e
kimgywls <65155823+kimgywls@users.noreply.github.com>
2024-08-28 18:05:10 +0900

    delete consol.log and update modal UI (#92)

--- lean_ai_front/src/pages/storeIntroduction.js
+++ lean_ai_front/src/pages/storeIntroduction.js
@@ -54,16 +54,16 @@ const StoreIntroduce = () => {
     <div className="flex justify-center items-center min-h-screen">
       <div className="border-blue-300 border p-5 rounded-lg shadow-lg flex flex-col items-center text-center mt-4 mb-2 w-10/12 h-3/6">
         <div className="rounded-lg p-8 w-full max-w-md text-center mb-2 relative">
-          <img src={`${config.localhosts}${storeData.store_image}`} alt="Store" className="mx-auto mb-4 w-64 h-52 object-contain" />
+          <img src={`${config.localhosts}${storeData.store_image}`} alt="Store" className="mx-auto mb-4  w-72 h-56 object-contain" />
           <p className="font-bold text-2xl">{storeData.store_name}</p>
         </div>
 
-        <div className="bg-sky-100 flex flex-col rounded-lg items-center text-center mb-4 w-64 pt-3 px-2">
-          <p className="mb-4 text-xl">{storeData.store_hours}</p>
+        <div className="bg-sky-100 flex flex-col rounded-lg items-center text-center mb-4 min-w-72 pt-3 px-2">
+          <p className="mb-4 text-xl whitespace-pre-line ">{storeData.store_hours}</p>
         </div>
 
-        <div className="bg-sky-100 flex flex-col rounded-lg items-center text-center w-64 mb-4 pt-3 px-2">
-          <p className="mt-2 mb-4 text-xl">{storeData.menu_prices}</p>
+        <div className="bg-sky-100 flex flex-col rounded-lg items-center text-center min-w-72 mb-4 pt-3 px-2">
+          <p className="mt-2 mb-4 text-xl whitespace-pre-line ">{storeData.menu_prices}</p>
         </div>
         
         {agentId && <Chatbot agentId={agentId} />} {/* agentId를 Chatbot 컴포넌트에 전달 */}
