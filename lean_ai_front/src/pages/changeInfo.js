669a9bbfd278fa14c44ae9c068f53e3dee792c95
kimgywls <65155823+kimgywls@users.noreply.github.com>
2024-08-28 14:39:21 +0900

    modify UI and notification

--- lean_ai_front/src/pages/changeInfo.js
+++ lean_ai_front/src/pages/changeInfo.js
@@ -47,10 +47,10 @@ export default function ChangeInfo({ }) {
         setStoreHours(firstStore.opening_hours || "");
         setMenuPrices(firstStore.menu_price || "");
         const bannerPath = firstStore.banner || "";
-        const storeImageUrl = bannerPath.startsWith("/media/") 
-            ? `${process.env.NEXT_PUBLIC_MEDIA_URL}${bannerPath}` 
-            : `${process.env.NEXT_PUBLIC_MEDIA_URL}/media/${bannerPath.replace(/^\/+/, '')}`;
-        
+        const storeImageUrl = bannerPath.startsWith("/media/")
+          ? `${process.env.NEXT_PUBLIC_MEDIA_URL}${bannerPath}`
+          : `${process.env.NEXT_PUBLIC_MEDIA_URL}/media/${bannerPath.replace(/^\/+/, '')}`;
+
         setStoreImage(storeImageUrl);
       } else {
         throw new Error('스토어 ID를 가져오지 못했습니다.');
@@ -91,7 +91,7 @@ export default function ChangeInfo({ }) {
     input.onchange = function (event) {
       if (event.target.files && event.target.files[0]) {
         const file = event.target.files[0];
-        setStoreImage(file); 
+        setStoreImage(file);
         closeImageModal();
       }
     };
@@ -155,11 +155,11 @@ export default function ChangeInfo({ }) {
   const saveAllChanges = useCallback(async () => {
     try {
       const formData = new FormData();
-  
-      formData.append('store_name', storeName || "");       
-      formData.append('opening_hours', storeHours || "");   
-      formData.append('menu_price', menuPrices || "");      
-  
+
+      formData.append('store_name', storeName || "");
+      formData.append('opening_hours', storeHours || "");
+      formData.append('menu_price', menuPrices || "");
+
       if (storeImage) {
         if (storeImage instanceof File) {
           formData.append('banner', storeImage);
@@ -172,7 +172,7 @@ export default function ChangeInfo({ }) {
           return;
         }
       }
-  
+
       const response = await fetch(`${config.localhosts}/api/user-stores/${storeId}/`, {
         method: 'PUT',
         headers: {
@@ -180,11 +180,11 @@ export default function ChangeInfo({ }) {
         },
         body: formData,
       });
-  
+
       if (!response.ok) {
         throw new Error('정보 저장에 실패했습니다.');
       }
-  
+
       const result = await response.json();
       setMessage('정보가 성공적으로 저장되었습니다.');
       setShowMessageModal(true);
@@ -195,163 +195,170 @@ export default function ChangeInfo({ }) {
   }, [storeId, storeName, storeHours, menuPrices, storeImage]);
 
   return (
-    <div className="bg-white flex flex-col items-center min-h-screen overflow-y-auto relative font-sans">
-      <div className="bg-white rounded-lg p-8 w-full max-w-md text-center mb-2">
-        <div className="flex items-center justify-center mb-4">
-          <Link href="/myPage" className="text-gray-500 mr-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 19l-7-7 7-7"
-              />
-            </svg>
-          </Link>
+    <div className="bg-white flex flex-col items-center justify-center min-h-screen overflow-y-auto relative font-sans mx-2">
+      <div className='flex flex-col justify-center items-center'>
+        <div className="bg-white rounded-lg p-8 w-full max-w-md text-center mb-2 ">
+          <div className="flex items-center justify-center mb-4">
+            <Link href="/mainPageForPresident" className="text-gray-500 mr-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </Link>
 
-          <div className="image-container relative inline-block">
-            <img
-              id="storeImage"
-              src={storeImage instanceof File ? URL.createObjectURL(storeImage) : storeImage}
-              className="mx-auto mb-4 w-64 h-52 object-contain cursor-pointer"
-              alt="Store"
-            />
-            <div
-              className="camera-icon absolute bottom-2 right-2 bg-white rounded-full p-2 shadow cursor-pointer"
-              onClick={openImageModal}
-            >
-              <CameraAltIcon />
+            <div className="image-container relative inline-block">
+              <img
+                id="storeImage"
+                src={storeImage instanceof File ? URL.createObjectURL(storeImage) : storeImage}
+                className="mx-auto mb-4 w-72 h-56 object-contain cursor-pointer"
+                alt="Store"
+              />
+              <div
+                className="camera-icon absolute bottom-2 right-2 bg-white rounded-full p-2 shadow cursor-pointer"
+                onClick={openImageModal}
+              >
+                <CameraAltIcon />
+              </div>
             </div>
           </div>
+          <div className="flex justify-center items-center">
+            <p id="storeName" className="font-bold text-2xl">
+              {storeName}
+            </p>
+            <button
+              onClick={() => openEditModal('storeName')}
+              className="ml-2 text-gray-500"
+            >
+              <EditIcon />
+            </button>
+          </div>
         </div>
-        <div className="flex justify-center items-center">
-          <p id="storeName" className="font-bold text-2xl">
-            {storeName}
+        <div className="bg-sky-100 flex flex-col items-center text-center mb-4 min-w-72 px-2 relative">
+          <p id="storeHours" className="mt-4 mb-4 text-xl whitespace-pre-line">
+            {storeHours}
           </p>
           <button
-            onClick={() => openEditModal('storeName')}
-            className="ml-2 text-gray-500"
+            onClick={() => openEditModal('storeHours')}
+            className="absolute top-2 right-2 text-gray-500"
           >
             <EditIcon />
           </button>
         </div>
-      </div>
-      <div className="bg-sky-100 flex flex-col items-center text-center mb-4 w-64 px-2 relative">
-        <p id="storeHours" className="mt-4 mb-4 text-xl whitespace-pre-line">
-          {storeHours}
-        </p>
-        <button
-          onClick={() => openEditModal('storeHours')}
-          className="absolute top-2 right-2 text-gray-500"
-        >
-          <EditIcon />
-        </button>
-      </div>
-      <div className="bg-sky-100 flex flex-col items-center text-center w-64 px-2 relative">
-        <p id="menuPrices" className="mt-4 mb-4 text-xl whitespace-pre-line">
-          {menuPrices}
-        </p>
-        <button
-          onClick={() => openEditModal('menuPrices')}
-          className="absolute top-2 right-2 text-gray-500"
-        >
-          <EditIcon />
-        </button>
-      </div>
-      <button
-        onClick={saveAllChanges}
-        className="mt-4 w-48 py-2 bg-blue-500 text-white text-center block rounded-full cursor-pointer"
-      >
-        모든 변경사항 저장
-      </button>
-
-      {isImageModalOpen && (
-        <div
-          id="imageModal"
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-        >
-          <div className="modal-content bg-white p-6 rounded-lg shadow-lg text-center">
-            <h2 className="text-2xl font-bold mb-4">배너 사진 설정</h2>
-            <button
-              onClick={chooseImage}
-              className="block w-full mb-2 py-2 text-black"
-            >
-              앨범에서 사진/동영상 선택
-            </button>
-            <button
-              onClick={applyDefaultImage}
-              className="block w-full py-2 text-black"
-            >
-              기본 이미지 적용
-            </button>
-            <button
-              onClick={closeImageModal}
-              className="block w-full py-2 mt-4 text-red-500"
-            >
-              취소
-            </button>
-          </div>
+        <div className="bg-sky-100 flex flex-col items-center text-center min-w-72 px-2 relative">
+          <p id="menuPrices" className="mt-4 mb-4 text-xl whitespace-pre-line">
+            {menuPrices}
+          </p>
+          <button
+            onClick={() => openEditModal('menuPrices')}
+            className="absolute top-2 right-2 text-gray-500"
+          >
+            <EditIcon />
+          </button>
         </div>
-      )}
-
-      {isEditModalOpen && (
-        <div
-          id="editModal"
-          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+        <button
+          onClick={saveAllChanges}
+          className="mt-4 w-48 py-2 bg-blue-500 text-white text-center block rounded-full cursor-pointer"
         >
-          <div className="modal-content bg-white p-6 rounded-lg shadow-lg text-center">
-            <h2 className="text-2xl font-bold mb-4">내용 수정</h2>
-            <textarea
-              id="editTextArea"
-              className="w-full h-32 p-2 border border-gray-300 rounded"
-              value={editText}
-              onChange={(e) => setEditText(e.target.value)}
-            />
-            <button
-              onClick={saveChanges}
-              className="block w-full py-2 mt-4 bg-blue-500 text-white rounded"
+          모든 변경사항 저장
+        </button>
+
+        {isImageModalOpen && (
+          <div
+            id="imageModal"
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          >
+            <div className="modal-content bg-white p-6 rounded-lg shadow-lg text-center "
+              style={{ width: '350px' }}
             >
+              <h2 className="text-2xl font-bold mb-4">배너 사진 설정</h2>
+              <button
+                onClick={chooseImage}
+                className="block w-full mb-2 py-2 text-black"
+              >
+                앨범에서 사진/동영상 선택
+              </button>
+              <button
+                onClick={applyDefaultImage}
+                className="block w-full py-2 text-black"
+              >
+                기본 이미지 적용
+              </button>
+              <button
+                onClick={closeImageModal}
+                className="block w-full py-2 mt-4 text-red-500"
+              >
+                취소
+              </button>
+            </div>
+          </div>
+        )}
+
+        {isEditModalOpen && (
+          <div
+            id="editModal"
+            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          >
+            <div className="modal-content bg-white p-6 rounded-lg shadow-lg text-center"
+                  style={{ width: '350px' }}
+            >
+              <h2 className="text-2xl font-bold mb-4">내용 수정</h2>
+              <textarea
+                id="editTextArea"
+                className="w-full h-32 p-2 border border-gray-300 rounded"
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+              />
+              <button
+                onClick={saveChanges}
+                className="block w-full py-2 mt-4 bg-blue-500 text-white rounded"
+              >
+                확인
+              </button>
+            </div>
+          </div>
+        )}
+
+        <ModalMSG
+          show={showMessageModal}
+          onClose={handleMessageModalClose}
+          title=" "
+          style={{ width: '350px' }}
+        >
+          <p style={{ whiteSpace: 'pre-line' }}>
+            {message}
+          </p>
+          <div className="flex justify-center mt-4">
+            <button onClick={handleMessageModalClose} className="text-white bg-blue-300 rounded-md px-4 py-2 font-normal border-l hover:bg-blue-500 ">
               확인
             </button>
           </div>
-        </div>
-      )}
+        </ModalMSG>
 
-      <ModalMSG
-        show={showMessageModal}
-        onClose={handleMessageModalClose}
-        title=" "
-      >
-        <p style={{ whiteSpace: 'pre-line' }}>
-          {message}
-        </p>
-        <div className="flex justify-center mt-4">
-          <button onClick={handleMessageModalClose} className="text-white bg-blue-300 rounded-md px-4 py-2 font-normal border-l hover:bg-blue-500 ">
-            확인
-          </button>
-        </div>
-      </ModalMSG>
-
-      <ModalErrorMSG
-        show={showErrorMessageModal}
-        onClose={handleErrorMessageModalClose}
-        title="Error"
-      >
-        <p style={{ whiteSpace: 'pre-line' }}>
-          {errorMessage}
-        </p>
-        <div className="flex justify-center mt-4">
-          <button onClick={handleErrorMessageModalClose} className="text-white bg-blue-300 rounded-md px-4 py-2 font-normal border-l hover:bg-blue-500 ">
-            확인
-          </button>
-        </div>
-      </ModalErrorMSG>
+        <ModalErrorMSG
+          show={showErrorMessageModal}
+          onClose={handleErrorMessageModalClose}
+          title="Error"
+        >
+          <p style={{ whiteSpace: 'pre-line' }}>
+            {errorMessage}
+          </p>
+          <div className="flex justify-center mt-4">
+            <button onClick={handleErrorMessageModalClose} className="text-white bg-blue-300 rounded-md px-4 py-2 font-normal border-l hover:bg-blue-500 ">
+              확인
+            </button>
+          </div>
+        </ModalErrorMSG>
+      </div>
     </div>
   );
 }
--- lean_ai_front/src/pages/chatBotMSG.jsx
+++ lean_ai_front/src/pages/chatBotMSG.jsx
@@ -4,14 +4,12 @@ export default function Chatbot({ agentId }) {
     const [sessionId, setSessionId] = useState(null);
 
     useEffect(() => {
-        // 매번 새로운 세션 ID 생성
         const newSessionId = `session-${Math.random().toString(36).substr(2, 9)}`;
         setSessionId(newSessionId);
 
         const resetSession = () => {
             const dfMessenger = document.querySelector('df-messenger');
             if (dfMessenger) {
-                // startNewSession을 호출하여 강제로 새 세션 시작
                 dfMessenger.setAttribute('session-id', newSessionId);
                 if (typeof dfMessenger.startNewSession === 'function') {
                     dfMessenger.startNewSession({ retainHistory: false });
@@ -19,7 +17,6 @@ export default function Chatbot({ agentId }) {
             }
         };
 
-        // DOM 요소가 렌더링된 후 resetSession을 호출
         setTimeout(resetSession, 500);
 
     }, [agentId]);
@@ -30,9 +27,9 @@ export default function Chatbot({ agentId }) {
                 <df-messenger
                     location="asia-northeast1"
                     project-id="lean-ai-faq"
-                    agent-id={agentId} // agentId를 여기에서 사용
+                    agent-id={agentId}
                     language-code="ko"
-                    session-id={sessionId} // 새로운 session-id를 설정
+                    session-id={sessionId}
                     session-persistence="false"
                     style={{ position: 'fixed', bottom: '20px', right: '15px' }}
                 >
@@ -45,8 +42,96 @@ export default function Chatbot({ agentId }) {
 
             <style jsx global>{`
                 df-messenger {
-                    --df-messenger-chat-window-width: 500%;
                     --df-messenger-chat-bubble-background: #fde047;
+                }
+
+                /* 기본 채팅창 크기 */
+                df-messenger {
+                    --df-messenger-chat-window-width: 500%
+                }
+
+                /* 디바이스 크기 */
+                @media (min-width: 300px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 450%;
+                        --df-messenger-chat-window-height: 800%;
+                    }
+                }
+                @media (min-width: 370px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 500%;
+                        --df-messenger-chat-window-height: 830%;
+                    }
+                }
+                @media (min-width: 400px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 550%;
+                        --df-messenger-chat-window-height: 900%;
+                    }
+                }
+                @media (min-width: 500px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 600%;
+                        --df-messenger-chat-window-height: 900%;
+                        --df-messenger-font-size: 16px;
+                    }
+                }
+                @media (min-width: 600px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 550%;
+                        --df-messenger-chat-window-height: 900%;
+                        --df-messenger-font-size: 25px;
+                    }
+                }
+                @media (min-width: 700px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 850%;
+                        --df-messenger-chat-window-height: 1180%;
+                    }
+                }
+                @media (min-width: 800px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 900%;
+                        --df-messenger-chat-window-height: 1300%;
+                        --df-messenger-font-size: 30px;
+                    }
+                }
+                @media (min-width: 900px) {
+                    df-messenger {
+                        --df-messenger-chat-window-width: 950%;
+                        --df-messenger-chat-window-height: 1460%;
+                    }
+                }
+                @media (min-width: 1000px) {
+                    @media (min-height: 500px) {
+                        df-messenger {
+                            --df-messenger-chat-window-width: 1100%;
+                            --df-messenger-chat-window-height: 690%;
+                            --df-messenger-font-size: 20px;
+                        }
+                    }
+                    @media (min-height: 600px) {
+                        df-messenger {
+                            --df-messenger-chat-window-width: 1100%;
+                            --df-messenger-chat-window-height: 750%;
+                            --df-messenger-font-size: 20px;
+                        }
+                    }
+                    @media (min-height: 700px) {
+                        df-messenger {
+                            --df-messenger-chat-window-width: 1100%;
+                            --df-messenger-chat-window-height: 850%;
+                            --df-messenger-font-size: 20px;
+                        }
+                    }
+
+                    @media (min-height: 800px) {
+                        df-messenger {
+                            --df-messenger-chat-window-width: 1100%;
+                            --df-messenger-chat-window-height: 1000%;
+                            --df-messenger-font-size: 20px;
+                        }
+                    }
                 }
             `}</style>
         </>
