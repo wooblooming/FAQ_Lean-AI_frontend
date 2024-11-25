422b856e4223de5e248247b204c403318e4741f0
kimgywls <wh980213@naver.com>
2024-11-25 10:33:06 +0900

    241125 update

--- lean_ai_front/src/contexts/authContext.js
+++ lean_ai_front/src/contexts/authContext.js
@@ -7,7 +7,7 @@ export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(null);
 
     useEffect(() => {
-        if (typeof window !== 'undefined') {
+        if (typeof window !== 'undefined' && !window.ReactNativeWebView) {
             const storedToken = sessionStorage.getItem('token');
             //console.log("Token loaded on page load:", storedToken);
             if (storedToken) {
@@ -19,14 +19,14 @@ export const AuthProvider = ({ children }) => {
     const saveToken = (newToken) => {
         //console.log("Saving new token:", newToken);
         setToken(newToken);
-        if (typeof window !== 'undefined') {
+        if (typeof window !== 'undefined' && !window.ReactNativeWebView) {
             sessionStorage.setItem('token', newToken);
         }
     };
 
     const removeToken = () => {
         setToken(null);
-        if (typeof window !== 'undefined') {
+        if (typeof window !== 'undefined' && !window.ReactNativeWebView) {
             sessionStorage.removeItem('token');
         }
     };
