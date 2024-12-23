445941f86112ee14ec5f5957bd2beefa88e6c12e
kimgywls <wh980213@naver.com>
2024-12-23 13:39:37 +0900

    241223 update 2

--- lean_ai_front/src/pages/news.js
+++ lean_ai_front/src/pages/news.js
@@ -16,10 +16,27 @@ export const news = [
   {
     id: 2,
     category: '보도자료',
+    title: `AI기술 활용 소상공인 업무 디지털 혁신 지원 '린에이아이'`,
+    date: '2024-09-03',
+    content: `▶ 창업하게 된 이유와 기업소개 그리고 앞으로 기업 운영 계획은? 
+▷‘린에이아이’는 창업 7년차 스타트업입니다. AI 기술로 지역·소득에 따른 교육 기회의 격차를 해소하고자 ‘잡쇼퍼’라는 회사명으로 창업한 기업입니다.`,
+    link: 'https://xn--zb0b20fnzw5rc.kr/sub_read.html?uid=18862&section=sc4&section2=%EB%8B%A8%EC%B2%B4%EC%86%8C%EC%8B%9D'
+  },
+  {
+    id: 3,
+    category: '보도자료',
     title: `린에이아이, 한국MS·중기부 '마중 프로그램' 선정`,
     date: '2024-09-24',
     content: `인공지능(AI) 전문 스타트업 린에이아이(대표 김하연, 옛 잡쇼퍼)가 한국마이크로소프트(MS)와 중소벤처기업부가 공동으로 진행하는 '마중 프로그램 5기'에 최종 선정됐다고 24일 밝혔다.`,
     link: 'https://www.etnews.com/20240924000231'
+  },
+  {
+    id: 4,
+    category: '보도자료',
+    title: `“소상공인을 위한 맞춤형 AI서비스 만들어드려요”···린에이아이의 착한 디지털 혁신`,
+    date: '2024-12-16',
+    content: `소상공인들의 업무 디지털 혁신을 도와 경쟁력 높여 맞춤형 AI 활용해 직원들 잡무 줄이고 만족도· 효율성 up AI챗봇 ‘무물(MUMUL)’, 소상공인 위한 ‘무엇이든 물어보세요’`,
+    link: 'https://www.mk.co.kr/news/economy/11195419'
   },
 ];
 
@@ -30,7 +47,7 @@ const NewsCard = ({ category, title, date, content, onClick }) => (
     onClick={onClick}
     initial={{ opacity: 0, scale: 0.9 }}
     animate={{ opacity: 1, scale: 1 }}
-    transition={{ duration: 0.5 }}
+    transition={{ duration: 0.7, delay: 0.5}}
   >
     <div className="bg-indigo-600 py-2 px-4">
       <span className="text-white text-lg font-semibold">{category}</span>
@@ -48,14 +65,17 @@ const Newsroom = () => {
   const [activeFilter, setActiveFilter] = useState('전체'); // 필터 상태
   const [currentPage, setCurrentPage] = useState(1); // 페이지 상태
   const filters = ['전체', '회사뉴스', '보도자료']; // 필터 옵션
-  const itemsPerPage = 6; // 페이지당 항목 수
-  const filteredNews = activeFilter === '전체' ? news : news.filter(item => item.category === activeFilter);
+  const itemsPerPage = 4; // 페이지당 항목 수
+  // 정렬된 news 데이터를 사용하기 위해 slice()로 원본 배열 변경 방지
+  const filteredNews = [...(activeFilter === '전체' ? news : news.filter(item => item.category === activeFilter))]
+    .sort((a, b) => new Date(b.date) - new Date(a.date));
   const pageCount = Math.ceil(filteredNews.length / itemsPerPage); // 페이지 수 계산
   const displayedNews = filteredNews.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
+
   return (
     <div className="bg-violet-50 min-h-screen p-8 text-indigo-600 flex items-center justify-center">
-      <div className="max-w-6xl bg-white rounded-lg p-4">
+      <div className="max-w-6xl bg-white rounded-lg p-4" style={{ minWidth: '1150px', minHeight: '700px' }}>
         {/* 헤더 및 뒤로가기 버튼 */}
         <div className='flex flex-row'>
           <ChevronLeft
@@ -70,7 +90,7 @@ const Newsroom = () => {
           className="flex justify-center space-x-4 mb-12"
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5, delay: 0.4 }}
+          transition={{ duration: 0.5, delay: 0.3}}
         >
           {filters.map((filter) => (
             <button
@@ -92,7 +112,7 @@ const Newsroom = () => {
         {/* 뉴스 카드 그리드 */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {displayedNews.map((item, index) => (
-            <NewsCard key={item.id} {...item} onClick={() => router.push(item.link)} />
+            <NewsCard key={item.id} {...item} onClick={() => window.open(item.link, '_blank')} />
           ))}
         </div>
 
