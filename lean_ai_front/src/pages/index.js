069af6185ac3a7f9393df99acfa31c339ed4452f
hjkim <wh980213@naver.com>
2025-06-23 09:52:40 +0900

    relative path

--- lean_ai_front/src/pages/index.js
+++ lean_ai_front/src/pages/index.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import AnimationComponent from '@/components/component/mumul'; // 애니메이션 컴포넌트
-import LandingPageContent from '@/components/component/landingPage/landingPageContent'; // 실제 랜딩 페이지 내용
+import React, { useState, useEffect } from "react";
+import AnimationComponent from "../components/component/mumul";
+import LandingPageContent from "../components/component/landingPage/landingPageContent";
 
 const LandingPage = () => {
   const [showLandingPage, setShowLandingPage] = useState(false);
 
   useEffect(() => {
     // sessionStorage에서 애니메이션 실행 여부 확인
-    const hasVisited = sessionStorage.getItem('hasVisited');
+    const hasVisited = sessionStorage.getItem("hasVisited");
 
     if (hasVisited) {
       // 이미 방문한 적이 있으면 바로 랜딩 페이지 표시
@@ -18,7 +18,7 @@ const LandingPage = () => {
   // 애니메이션이 끝나면 랜딩 페이지를 표시하고, sessionStorage에 값 저장
   const handleAnimationEnd = () => {
     setShowLandingPage(true);
-    sessionStorage.setItem('hasVisited', 'true'); // 애니메이션 실행됨을 표시
+    sessionStorage.setItem("hasVisited", "true"); // 애니메이션 실행됨을 표시
   };
 
   return (
